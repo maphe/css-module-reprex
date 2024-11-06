8fa8a011f01c3036067329dd97bd65a9c1b7b136
maphe <mathieu@pwalabs.com>
2024-11-06 12:35:28 -0800

    Client state management

--- src/lib/ui/src/lib/ClientComponent.tsx
+++ src/lib/ui/src/lib/ClientComponent.tsx
@@ -2,11 +2,16 @@
 
 import styles from './ClientComponent.module.scss';
 import { SubComponent } from './SubComponent';
+import { useState } from 'react';
+import clsx from 'clsx';
 
 export const ClientComponent = () => {
+  const [open, setOpen] = useState(false)
+
   return (
     <div className={styles.container}>
-      <h2>Client Component</h2>
+      <h2 className={clsx(styles.title, {[styles.open]: open})}>Client Component</h2>
+      <button onClick={() => setOpen(!open)}>Toggle Open ({open?"open":"closed"})</button>
       <SubComponent />
     </div>
   );
