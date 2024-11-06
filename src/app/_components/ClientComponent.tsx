5404611a2d9ec67ad898a48bd6edc28a1d36fd27
maphe <mathieu@pwalabs.com>
2024-11-06 11:07:51 -0800

    Reprex code

---
+++ src/app/_components/ClientComponent.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import styles from './ClientComponent.module.scss';
+
+export const ClientComponent = () => {
+  return (
+    <div className={styles.container}>
+      <h2>Client Component</h2>
+    </div>
+  );
+}
