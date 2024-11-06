1867fc18ef7d8e050a9a6b0131d35a34d5ad86f6
maphe <mathieu@pwalabs.com>
2024-11-06 14:14:51 -0800

    Add static function in subcomponent

--- src/lib/ui/src/lib/SubComponent.tsx
+++ src/lib/ui/src/lib/SubComponent.tsx
@@ -1,9 +1,19 @@
 import styles from './SubComponent.module.scss';
 
+const formatCount = (num: number): string => {
+  if (num >= 1_000_000) {
+    return (num / 1_000_000).toFixed(1) + "M"
+  } else if (num >= 1_000) {
+    return (num / 1_000).toFixed(1) + "k"
+  } else {
+    return num.toString()
+  }
+}
+
 export const SubComponent = () => {
   return (
     <div className={styles.container}>
-      <h3>SubComponent</h3>
+      <h3>SubComponent {formatCount(8364893746)}</h3>
     </div>
   );
 }
