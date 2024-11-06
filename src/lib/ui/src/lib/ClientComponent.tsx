ff9124c2dd157efc087c2d34fd15e4fca6928b40
maphe <mathieu@pwalabs.com>
2024-11-06 12:43:15 -0800

    Add view transition

--- src/lib/ui/src/lib/ClientComponent.tsx
+++ src/lib/ui/src/lib/ClientComponent.tsx
@@ -4,6 +4,7 @@ import styles from './ClientComponent.module.scss';
 import { SubComponent } from './SubComponent';
 import { useState } from 'react';
 import clsx from 'clsx';
+import { flushSync } from 'react-dom';
 
 export const ClientComponent = () => {
   const [open, setOpen] = useState(false)
@@ -11,7 +12,11 @@ export const ClientComponent = () => {
   return (
     <div className={styles.container}>
       <h2 className={clsx(styles.title, {[styles.open]: open})}>Client Component</h2>
-      <button onClick={() => setOpen(!open)}>Toggle Open ({open?"open":"closed"})</button>
+      <button onClick={() => {
+        document.startViewTransition(() => {
+          flushSync(() => setOpen(!open))
+        })
+      }}>Toggle Open ({open?"open":"closed"})</button>
       <SubComponent />
     </div>
   );
